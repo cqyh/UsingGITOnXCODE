36f47f5355fe8e0cc5dfe62870827421954ea008
Christopher Quah <christopherquah@Larrys-MacBook-Pro.local>
2018-04-18 11:41:46 +0800

    Something

---
+++ GITHUB over9000/GITHUB over9000/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GITHUB over9000
+//
+//  Created by Christopher Quah on 12/4/18.
+//  Copyright © 2018 Christopher Quah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GITHUB over9000/GITHUB over9000Tests/GITHUB_over9000Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GITHUB_over9000Tests.swift
+//  GITHUB over9000Tests
+//
+//  Created by Christopher Quah on 12/4/18.
+//  Copyright © 2018 Christopher Quah. All rights reserved.
+//
+
+import XCTest
+@testable import GITHUB_over9000
+
+class GITHUB_over9000Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
